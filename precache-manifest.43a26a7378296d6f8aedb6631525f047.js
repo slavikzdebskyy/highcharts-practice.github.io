0d505c3aa767b9d0fe2576ac344653e647b3f8fb
slavikzdebskyy <zdebskyy.slavik@gmail.com>
2018-11-21 06:14:03 +0200

    init

---
+++ precache-manifest.43a26a7378296d6f8aedb6631525f047.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "505dcb09190ff040b318",
+    "url": "/static/js/main.505dcb09.chunk.js"
+  },
+  {
+    "revision": "d262dada2c659e097b6b",
+    "url": "/static/js/1.d262dada.chunk.js"
+  },
+  {
+    "revision": "505dcb09190ff040b318",
+    "url": "/static/css/main.c943bc39.chunk.css"
+  },
+  {
+    "revision": "5843ba4c1c835d1c7fab0be011251add",
+    "url": "/index.html"
+  }
+];
